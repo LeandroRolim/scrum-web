745ec096fe8f00056bd77972efa951058442cf7e
Leandro Rolim <leandro@viatechno.com.br>
2020-10-25 11:32:49 -0300

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('base')
+
+@section('content')
+    <h1>estamos na home</h1>
+
+    <livewire:counter />
+@endsection
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h3>primeiro exemplo com livewire</h3>
+    <button wire:click="increment">+</button>
+    Contador: {{ $count }}
+</div>
