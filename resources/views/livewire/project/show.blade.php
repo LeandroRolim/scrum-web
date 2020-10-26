a256c2daaf221626eb883db8a2b8bee00a2e82de
Leandro Rolim <leandro@viatechno.com.br>
2020-10-26 16:52:41 -0300

    laravel jetstream install

---
+++ resources/views/livewire/project/show.blade.php
@@ -0,0 +1,11 @@
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ $project->name }}
+    </h2>
+</x-slot>
+
+<div>
+    <div class="container mx-auto rounded shadow mt-4 p-4 bg-white">
+        <form></form>
+    </div>
+</div>
