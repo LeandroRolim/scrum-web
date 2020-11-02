6e0a5dcafa56b85c1cc68255e5b820bd5acffc16
Leandro Rolim <leandro@viatechno.com.br>
2020-11-02 18:39:43 -0300

    wip: voting

--- app/Http/Livewire/Project/VotingComponent.php
+++ app/Http/Livewire/Project/VotingComponent.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Livewire\Project;
 
+use App\Models\Project;
 use Livewire\Component;
 
 class VotingComponent extends Component
 {
+    public Project $project;
+
+    public ?string $task_selected = null;
+
+    public function getTaskProperty()
+    {
+        return $this->project->tasks;
+    }
+
     public function render()
     {
         return view('livewire.project.voting-component');
--- resources/views/livewire/project/voting-component.blade.php
+++ resources/views/livewire/project/voting-component.blade.php
@@ -1,13 +1,25 @@
 <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight flex justify-between">
         {{ __('Voting') }}
-        <a href="{{ route('project.create') }}" class="button secondary">
-            {{ __('Start') }}
-            <i class="fas fa-hourglass-start"></i>
-        </a>
     </h2>
 </x-slot>
 
-<div>
-    <form action=""></form>
+<div class="container bg-white rounded shadow my-4 p-4 mx-auto">
+    <form class="grid gap-4">
+        <div>
+            <select class="shadow-sm rounded-md form-input w-full" wire:model.defer="task_selected">
+                <option value="">{{ __('No selection') }}</option>
+                @foreach ($this->tasks as $task)
+                    <option value="{{ $task->id }}">{{ $task->task }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <x-jet-secondary-button>
+                {{ __('Start') }}
+                <i class="fas fa-hourglass-start"></i>
+            </x-jet-secondary-button>
+        </div>
+
+    </form>
 </div>
